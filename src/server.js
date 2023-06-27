945a9d4293d485f695d8a7e65822828f4b02424a
Marcelo dos Santos <marcelinho.cost@gmail.com>
2023-06-27 18:03:00 -0300

    Third Commit

--- src/server.js
+++ src/server.js
@@ -10,11 +10,11 @@ database();
 const routes = require("./routes");
 
 const app = express();
+app.use(cors())  
 app.use(express.json());
 
 app.use(routes);
 
-app.use(cors()) 
 
 
 app.use((error, request, response, next) => {
